8b8e21d66776de87c696b11fa85c392f747f04c6
pereiradaniel <danielpaulpereira@protonmail.com>
2021-05-26 13:25:00 -0400

    Update modules

--- lecture_notes_26may/m1.js
+++ lecture_notes_26may/m1.js
@@ -1,3 +1,14 @@
 var localFunction = function() {
-    module.export.msg = "Hello";
+}
+
+var msg2 = "Daniel";
+
+module.exports.msg = "msg";
+
+module.exports.writeMsg = function() {
+    console.log("Hello " + msg2);
+}
+
+module.exports.writeMsg2 = function(name) {
+    console.log("Hello " + name);
 }
--- lecture_notes_26may/server.js
+++ lecture_notes_26may/server.js
@@ -1,5 +1,10 @@
 var m1 = require("./m1");
 
+m1.writeMsg();
+m1.writeMsg2("Daniel");
+
+console.log(m1.msg);
+
 console.log("Hello");
 console.log(__dirname);
 console.log(__filename);
